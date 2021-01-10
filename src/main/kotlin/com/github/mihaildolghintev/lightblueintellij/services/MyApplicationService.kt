7c16e5208d55a461b4f3b7b536dea3c29a47c66b
GitHub Action <action@github.com>
2021-01-10 09:05:34 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mihaildolghintev/lightblueintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mihaildolghintev.lightblueintellij.services
+
+import com.github.mihaildolghintev.lightblueintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mihaildolghintev/lightblueintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mihaildolghintev/lightblueintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mihaildolghintev.lightblueintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mihaildolghintev.lightblueintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
